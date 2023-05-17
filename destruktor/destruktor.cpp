019c5fee93e10dc4fb1dfae49bde06b4aeacdcc4
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-05-17 13:16:50 +0700

    set akses ke private yang memiliki 2 member salah satunya member pointer. lalu set akses ke publik syntak angka(int) merupakan konstruktor dan ~angka() merupakan destruktor

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -11,37 +11,3 @@ public:
 	void cetakData();
 	void isiData();
 };
-//definisi member function
-angka::angka(int i) {  //construktor
-	panjang = i;
-	arr = new int[1];
-	isiData();
-}
-
-angka::~angka() {   //destructor
-	cout << endl;
-	cetakData();
-	delete[]arr;
-	cout << "Alamat array sudah di lepaskan" << endl;
-}
-
-void angka::cetakData() {
-	for (int i = 1;i <= panjang;i++) {
-		cout << i << " = " << arr[i] << endl;
-	}
-}
-
-void angka::isiData() {
-	for (int i = 1;i <= panjang;i++) {
-		cout << i << " = ";cin >> arr[i];
-	}
-	cout << endl;
-}
-
-int main() {
-	angka belajarcpp(3);   //construktor di panggil
-	angka* ptrBelajarcpp = new angka(5); //construktor di panggil
-	delete ptrBelajarcpp; //destructor di panggil
-
-	return 0;
-}//destruktor di panggil
