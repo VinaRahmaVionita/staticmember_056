5240b9a246d07c6b51864695487aba9f7fbd7f92
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-05-17 13:12:09 +0700

    menambahkan void mahasiswa untuk menghasilkan output nim dan nama

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -27,3 +27,12 @@ mahasiswa::mahasiswa(string iNama) {
 	nama = iNama;
 }
 
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama = iNama;
+}
+void mahasiswa::cetak() {
+	cout << endl << "Nim =" << nim << endl;
+	cout << "Nama =" << nama << endl;
+}
+
